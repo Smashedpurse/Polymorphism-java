c7cf110b6f1303328b1e8598cf898b54ea66e885
Ivan Rubio <smashedpurse@gmail.com>
2022-12-28 17:08:16 -0600

    Apply the polymorphism in an array and see the results

--- src/com/cofacademy/example/Main.java
+++ src/com/cofacademy/example/Main.java
@@ -3,15 +3,28 @@ package com.cofacademy.example;
 public class Main {
     public static void main(String[] args) {
 
+        //DECLARACIÓN DE OBJETO DE TIPO ANIMAL (SUPER CLASE) CON POLIMOFISMO
          Animal  animal = new Dog();
          Animal animalc = new Cat();
 
-         animal.eat();
+         /*animal.eat();
          animal.sleep();
          animal.attack();
          animalc.eat();
          animalc.sleep();
          animalc.attack();
+          */
 
+
+         Animal [] animalArray = new Animal[4];
+
+         animalArray[0]= new Animal();
+         animalArray[1]= new Dog();
+         animalArray[2]= new Cat();
+         animalArray[3]= new Dog();
+
+         for (Animal a : animalArray ) {
+             a.eat();
+         }
     }
 }
